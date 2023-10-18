7fb5803322d605cb73ee876fc99978180166a223
Camilo Cuitiva <camiloandres174@gmail.com>
2023-10-18 16:46:58 -0500

    feat: Add home sections

---
+++ src/dataProjects.js
@@ -0,0 +1,226 @@
+const projects = [
+  {
+    id: 1,
+    name: "Cerasus Monteblanco",
+    price: "Desde $191.400.000",
+    area: "Desde 44 m2",
+    city: "Bogotá",
+    type: "vis",
+    text: "Un nuevo Proyecto de Vivienda de Interés Social en Bogotá, un proyecto VIS A OTRO NIVEL.",
+    descriptionType: "Vivienda de interés social",
+    logo: "./src/assets/Cerasus_Monteblanco/Logo_Cerasus_Monteblanco.png",
+    fachada: "./src/assets/Cerasus_Monteblanco/Fachada_Cerasus_Monteblanco.jpg",
+    fachadaAlt: "Fachada Cerasus Monteblanco",
+    logoAlt: "Logo Cerasus Monteblanco",
+    link: "/projects/cerasus-monteblanco",
+    note: "*Precio de 150 SMMLV proyectado al año de escrituración",
+    features: [
+      {
+        name: "Gimnasio",
+        icon: "./src/assets/Gimnasio-apartamento.png",
+      },
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+      {
+        name: "Centro de negocios",
+        icon: "./src/assets/Business-center-apartamento.png",
+      },
+      {
+        name: "Luoteca",
+        icon: "./src/assets/Ludoteca-apartamento.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Cerasus Danubio",
+    price: "Desde $191.400.000",
+    area: "Desde 33 m2",
+    city: "Bogotá",
+    type: "vis",
+    text: "Un nuevo concepto de construcción enfocado en dirigir el proyecto hacia un factor humano mas que monetario.",
+    descriptionType: "Vivienda de interés social",
+    logo: "./src/assets/Cerasus_Danubio/Logo_Cerasus_Danubio.png",
+    fachada: "./src/assets/Cerasus_Danubio/Fachada_Cerasus_Danubio.png",
+    fachadaAlt: "Fachada Cerasus Danubio",
+    logoAlt: "Logo Cerasus Danubio",
+    link: "/projects/cerasus-danubio",
+    note: "*Precio de 150 SMMLV proyectado al año de escrituración.",
+    features: [
+      {
+        name: "Zona de Fogata",
+        icon: "./src/assets/Zona-de-fogatas-apartamento.png",
+      },
+      {
+        name: "Coffee Work",
+        icon: "./src/assets/Coffee-Work-apartamento.png",
+      },
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+      {
+        name: "Terraza comunal",
+        icon: "./src/assets/Terraza-comunal-apartamento.png",
+      },
+      {
+        name: "Luoteca",
+        icon: "./src/assets/Ludoteca-apartamento.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Cerasus Usme",
+    price: "Desde $104.400.000",
+    area: "Desde 41 m2",
+    city: "Bogotá",
+    type: "vip",
+    text: "Unn proyecto de vivienda VIP (interés prioritario) construido en la Calle 110 A Sur No 3 A- 40 en la Localidad de Usme.",
+    descriptionType: "Vivienda de interés prioritario",
+    logo: "./src/assets/Cerasus_Usme/Logo_Cerasus_Usme.png",
+    fachada: "./src/assets/Cerasus_Usme/Fachada_Cerasus_Usme.jpg",
+    fachadaAlt: "Fachada Cerasus Usme",
+    logoAlt: "Logo Cerasus Usme",
+    link: "/projects/cerasus-usme",
+    note: "*Precio de 90 SMMLV proyectado al año de escrituración.",
+    features: [
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Zona de Fogata",
+        icon: "./src/assets/Zona-de-fogatas-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+      {
+        name: "Zona de trote",
+        icon: "./src/assets/Zona-de-trote-apartamento.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Avium Tunja",
+    price: "Desde $200.000.000",
+    area: "Desde 45 m2",
+    city: "Tunja",
+    type: "no vis",
+    text: "Es proyecto de vivienda de estrato 3 constrido en la calle 18 No. 1-140 del sector del Rodeo en la ciudad de Tunja.",
+    descriptionType: "Vivienda no VIS",
+    logo: "./src/assets/Avium_Tunja/Logo_Avium_Tunja.png",
+    fachada: "./src/assets/Avium_Tunja/Fachada_Avium_Tunja.jpg",
+    fachadaAlt: "Fachada Avium Tunja",
+    logoAlt: "Logo Avium Tunja",
+    link: "/projects/avium-tunja",
+    note: "*Precio sujeto a cambios",
+    features: [
+      {
+        name: "Piscina",
+        icon: "./src/assets/Piscina-apartamento.png",
+      },
+      {
+        name: "Gimnasio",
+        icon: "./src/assets/Gimnasio-apartamento.png",
+      },
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+      {
+        name: "Terraza comunal",
+        icon: "./src/assets/Terraza-comunal-apartamento.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Cerasus Sexta Entrada",
+    price: "Desde $99.200.000",
+    area: "Desde 46 m2",
+    city: "Malambo",
+    type: "vip",
+    text: "Un nuevo Proyecto de Vivienda de Interés Prioritario en Barranquilla.",
+    descriptionType: "Vivienda de interés prioritario",
+    logo: "./src/assets/Cerasus_Sexta_Entrada/Logo_Cerasus_Sexta_Entrada.png",
+    fachada:
+      "./src/assets/Cerasus_Sexta_Entrada/Fachada_Cerasus_Sexta_Entrada.png",
+    fachadaAlt: "Fachada Cerasus Sexta Entrada",
+    logoAlt: "Logo Cerasus Sexta Entrada",
+    link: "/projects/cerasus-sexta-entrada",
+    note: "*Precio de 90 SMMLV proyectado al año de escrituración.",
+    features: [
+      {
+        name: "Piscina",
+        icon: "./src/assets/Piscina-apartamento.png",
+      },
+      {
+        name: "Gimnasio",
+        icon: "./src/assets/Gimnasio-apartamento.png",
+      },
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Prunus Sexta Entrada",
+    price: "Desde $132.250.000",
+    area: "Desde 53 m2",
+    city: "Malambo",
+    type: "vis",
+    text: "Un nuevo Proyecto de Vivienda de Interés Social en Barranquilla.",
+    descriptionType: "Vivienda de interés social",
+    logo: "./src/assets/Prunus_Sexta_Entrada/Logo_Prunus_Sexta_Entrada.png",
+    fachada:
+      "./src/assets/Prunus_Sexta_Entrada/Fachada_Prunus_Sexta_Entrada.png",
+    fachadaAlt: "Fachada Prunus Sexta Entrada",
+    logoAlt: "Logo Prunus Sexta Entrada",
+    link: "/projects/prunus-sexta-entrada",
+    note: "*Precio de 115 SMMLV proyectado al año de escrituración.",
+    features: [
+      {
+        name: "Piscina",
+        icon: "./src/assets/Piscina-apartamento.png",
+      },
+      {
+        name: "Gimnasio",
+        icon: "./src/assets/Gimnasio-apartamento.png",
+      },
+      {
+        name: "Parque infantil",
+        icon: "./src/assets/Salon-de-ninos-apartamento.png",
+      },
+      {
+        name: "Salón social",
+        icon: "./src/assets/Salon-social-apartamento.png",
+      },
+    ],
+  },
+];
+
+export default projects;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,50 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        shark: {
+          50: "#f4f6f7",
+          100: "#e2e8eb",
+          200: "#c8d3d9",
+          300: "#a2b5be",
+          400: "#748e9c",
+          500: "#597281",
+          600: "#4c5f6e",
+          700: "#42505c",
+          800: "#3c464e",
+          900: "#23282d",
+        },
+        amber: {
+          50: "#ffffea",
+          100: "#fffbc5",
+          200: "#fff885",
+          300: "#ffee46",
+          400: "#ffdf1b",
+          500: "#ffc107",
+          600: "#e29400",
+          700: "#bb6902",
+          800: "#985108",
+          900: "#7c420b",
+        },
+        gray: {
+          50: "#FAFAFC",
+          100: "#E9E9EC",
+          200: "#C6C8CD",
+          300: "#ACAEB6",
+          400: "#92959F",
+          500: "#777C87",
+          600: "#5D6370",
+          700: "#434959",
+          800: "#293041",
+          900: "#0f172a",
+        },
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
